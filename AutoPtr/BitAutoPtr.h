84c7a3700f1bf33e27e2cdc5eea8b8c3077e5c8c
Luoao <luohao.me@outlook.com>
2020-03-21 19:57:29 +0800

    Update BitAutoPtr.h

--- AutoPtr/BitAutoPtr.h
+++ AutoPtr/BitAutoPtr.h
@@ -1,6 +1,17 @@
 #ifndef BIT_AUTO_PTR_H_
 #define BIT_AUTO_PTR_H_
 
+#define BIT_AUTO_PTR_MALLOC_ERR               1
+
+class BitAutoPtrLock
+{
+public:
+   BitAutoPtrLock() {}
+   ~BitAutoPtrLock() {}
+   void Lock() {}
+   void UnLock() {}
+};
+
 template<class T>
 class BitAutoPtr
 {
@@ -8,103 +19,153 @@ private:
    class Data
    {
    public:
-      int m_count;
-      T *m_pT;
+      int  m_count;
+      T    *m_pT;
+   private:
+      BitAutoPtrLock m_lock;
    public:
       Data() : m_count(0), m_pT(NULL) {}
+      void Lock() { m_lock.Lock(); }
+      void UnLock() { m_lock.UnLock(); }
    };
 
 private:
    Data *m_pData;
-   Data *GetData() const { return m_pData; }
-   Data *GetNewData() const
-   {
+   BitAutoPtrLock m_lock;
+   bool m_isOnlyShare;
+
+   void Lock() { m_lock.Lock(); }
+   void UnLock() { m_lock.UnLock();  }
+
+private:
+
+   Data *GetNewData() const {
       Data *pData = new Data();
-      if (pData == NULL)
-      {
-         throw "New faild";
+      if (pData == NULL) {
+         throw BIT_AUTO_PTR_MALLOC_ERR;
       }
       pData->m_count = 1;
       pData->m_pT = NULL;
       return pData;
    }
 
-public:
-   BitAutoPtr() : m_pData(NULL) {}
-   BitAutoPtr(const BitAutoPtr<T> &bitPtr)
-   {
+   void Assign(const BitAutoPtr<T> &bitPtr) {
+      BitAutoPtr<T> *pBitPtr = (BitAutoPtr<T> *)&bitPtr;
+      m_pData = pBitPtr->AddRef();
+      m_isOnlyShare = pBitPtr->IsShare();
+   }
+
+   void Init() { m_pData = NULL; m_isOnlyShare = false;  }
+
+   void ReleaseData() {
+      if (m_pData == NULL) {
+         return;
+      }
+      m_pData->Lock();
+
+      m_pData->m_count -= 1;
+      if (m_pData->m_count <= 0) {
+         if (m_pData->m_pT != NULL) {
+            delete m_pData->m_pT;
+            m_pData->m_pT = NULL;
+         }
+         delete m_pData;
+      }
+      m_pData->UnLock();
       m_pData = NULL;
+   }
+
+   BitAutoPtr<T> Clone() const {
+      BitAutoPtr<T> ret;
+      if (m_pData != NULL) {
+         if (m_pData->m_pT != NULL) {
+            T *pT = new T();
+            if (pT == NULL) {
+               throw BIT_AUTO_PTR_MALLOC_ERR;
+            }
+            ret = BitAutoPtr<T>(pT);
+            *pT = *m_pData->m_pT;
+         }
+      }
+      return ret;
+   }
+
+public:
+   bool IsShare() { return m_isOnlyShare; }
+
+   Data *AddRef() {
+      if (m_pData == NULL) {
+         return NULL;
+      }
+      m_pData->Lock();
+      m_pData->m_count++;
+      m_pData->UnLock();
+      return m_pData;
+   }
+
+   unsigned int GetCount() {
+      int ret = 0;
+      Lock();
+      if (m_pData == NULL) {
+         ret = 0;
+      } else {
+         ret = m_pData->m_count;
+      }
+      UnLock();
+      return ret;
+   }
+
+   BitAutoPtr() { Init(); }
+
+   BitAutoPtr(const BitAutoPtr<T> &bitPtr) {
+      Init();
       *this = bitPtr;
    }
-   BitAutoPtr(T *pT)
-   {
+
+   BitAutoPtr(T *pT, bool isShare = false) {
+      Init();
       m_pData = GetNewData();
       m_pData->m_pT = pT;
+      m_isOnlyShare = isShare;
    }
 
-   ~BitAutoPtr()
-   {
-      if (m_pData != NULL)
-      {
-         m_pData->m_count = m_pData->m_count - 1;
-         if (m_pData->m_count <= 0)
-         {
-            if (m_pData->m_pT != NULL)
-            {
-               delete m_pData->m_pT;
-               m_pData->m_pT = NULL;
-            }
-            delete m_pData;
-            m_pData = NULL;
-         }
-      }
+   ~BitAutoPtr() {
+      Lock();
+      ReleaseData();
+      UnLock();
    }
 
-   BitAutoPtr<T> Clone()
-   {
-      if (m_pData != NULL && m_pData->m_pT != NULL)
-      {
-         T *pT = new T();
-         if (pT == NULL)
-         {
-            throw "New faild";
-         }
-         BitAutoPtr<T> bitPtr(pT);
-         *pT = *m_pData->m_pT;
-         return bitPtr;
+   BitAutoPtr<T> &operator= (const BitAutoPtr<T> &bitPtr) {
+      Lock();
+      if (m_pData != NULL) {
+         ReleaseData();
       }
-      else
-      {
-         return BitAutoPtr<T>();
-      }
-   }
-
-   BitAutoPtr<T> &operator=(const BitAutoPtr<T> &bitPtr)
-   {
-      if (m_pData != NULL)
-      {
-         m_pData->m_count = m_pData->m_count - 1;
-      }
-      Data *pData = bitPtr.GetData();
-      if (pData != NULL)
-      {
-         m_pData = pData;
-         m_pData->m_count = m_pData->m_count + 1;
-      }
+      Assign(bitPtr);
+      UnLock();
       return *this;
    }
 
-   T *operator->()
-   {
-      if (m_pData->m_count > 1)
-      {
-         *this = Clone();
+   T *operator->() {
+      BitAutoPtr<T> clonePtr;
+      bool isClone = false;
+
+      m_pData->Lock();
+      if (m_pData->m_count > 1) {
+         m_pData->m_count -= 1;
+         isClone = true;
+         clonePtr = Clone();
+      }
+      m_pData->UnLock();
+
+      if (isClone) {
+         Lock();
+         Assign(clonePtr);
+         UnLock();
       }
       return m_pData->m_pT;
    }
 
-   const T *operator->() const
-   {
+   const T *operator->() const {
       return m_pData->m_pT;
    }
 };
